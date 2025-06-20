1df2db046ba43219e4f36ee9d37b1025f365db99
Haridugrani <bhargavdungrani32@gmail.com>
2025-06-20 14:28:51 +0530

    file changes

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 console.log("Hello, World!");
+
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRoutes = require('./routes/userRoutes');
+const connectRedis = require('./config/redis');
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('✅ MongoDB Connected'))
+  .catch(err => console.error('MongoDB error:', err));
+
+
+connectRedis();
+
+app.use('/api/users', userRoutes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error('Error:', err);
+    res.status(500).json({ message: 'Internal Server Error' });
+    }
+);
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the API!');
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
